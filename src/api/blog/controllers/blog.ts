99e1c3841438ce413441a228eefd43d7ef38a773
kagomen <fu.taz.1102@gmail.com>
2025-06-23 11:32:12 +0900

    Controllerのカスタマイズ

--- src/api/blog/controllers/blog.ts
+++ src/api/blog/controllers/blog.ts
@@ -1,7 +1,43 @@
+import { factories } from "@strapi/strapi"
+
 /**
- * blog controller
+ * Strapiのコアコントローラーを生成し、カスタムメソッドで拡張するためのファクトリ関数
+ *
+ * @param {string} uid 対象となるコンテンツタイプのユニークID（例: api::blog.blog）
+ * @param {object} strapi Strapiのグローバルインスタンス。他のAPIやサービスにアクセスする際に使用する
+ * @param {function({ strapi: object }): object} callback コントローラーを拡張するためのコールバック関数
+ * @returns デフォルトの機能とカスタム機能がマージされた、最終的なコントローラーオブジェクト
  */
+export default factories.createCoreController(
+  "api::blog.blog",
+  ({ strapi }) => ({
+    // findOne()をオーバーライド
+    async findOne(ctx) {
+      // 元のfindOne()を呼び出して基本的なデータを取得
+      const response = await super.findOne(ctx)
 
-import { factories } from '@strapi/strapi'
+      // レスポンスデータが存在する場合のみ、追加の処理を行う
+      if (response.data) {
+        const content = response.data.content || ""
+        const wordCount = content.length
+        response.data.wordCount = wordCount
+      }
 
-export default factories.createCoreController('api::blog.blog');
+      return response
+    },
+
+    // find()をオーバーライド
+    async find(ctx) {
+      const response = await super.find(ctx)
+
+      if (response.data && Array.isArray(response.data)) {
+        response.data.forEach((entry) => {
+          const content = entry.content || ""
+          entry.wordCount = content.length
+        })
+      }
+
+      return response
+    },
+  })
+)
