e3bbc036addf3ba34cc3f716212fc27745d3ed75
kagomen <fu.taz.1102@gmail.com>
2025-06-24 14:56:02 +0900

    cors設定

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,21 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["http://localhost:3000"],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      keepHeaderOnError: true,
+    },
+  },
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+]
