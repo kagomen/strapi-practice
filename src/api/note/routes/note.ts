a61644b5d760e80230aa4c8806dd68e2c28e8d29
kagomen <fu.taz.1102@gmail.com>
2025-06-26 15:14:42 +0900

    middlewareの適用を通常のルーターに移動

--- src/api/note/routes/note.ts
+++ src/api/note/routes/note.ts
@@ -1,7 +1,18 @@
-/**
- * note router
- */
-
 import { factories } from "@strapi/strapi"
 
-export default factories.createCoreRouter("api::note.note")
+// デフォルトのCRUD操作にはない、独自のURLパスやエンドポイントを作成したいときにカスタムルーターを使用する
+// 今回はデフォルトCRUD操作をカスタムするだけなので、カスタムルーターを作成する必要はなく、
+// コアルーターを編集するだけとする
+export default factories.createCoreRouter("api::note.note", {
+  config: {
+    update: {
+      middlewares: ["api::note.is-owner"],
+    },
+    delete: {
+      middlewares: ["api::note.is-owner"],
+    },
+    findOne: {
+      middlewares: ["api::note.is-owner"],
+    },
+  },
+})
